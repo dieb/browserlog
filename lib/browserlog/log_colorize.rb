9392d1d1275a74c0430530c3b8a81862e1e503a3
Vítor Avelino <contact@vitoravelino.me>
2014-05-14 22:50:52 -0400

    Added regex to parse sql load and rendered within layout lines

--- lib/browserlog/log_colorize.rb
+++ lib/browserlog/log_colorize.rb
@@ -2,15 +2,21 @@ module Browserlog
   class LogColorize
     REXP_REQUEST    = /^Started (?<method>\w+) "(?<path>[\w\/\?\&\=\.]+)" for (?<ip>[\d\.]+) at (?<date>[\w:\-\s]+)$/
     REXP_CONTROLLER = /^Processing by (?<controller>[\w\:]+)#(?<action>\w+) as (?<format>\w+)$/
-    REXP_RENDER =     /^\s*Rendered (?<path>.*\/)(?<template_name>\w+\.html\.\w+) \((?<rendering_time>[\d+\.]+ms)\)$/
-    REXP_COMPLETE =   /^Completed (?<status>\d+\s\w+) in (?<total_time>[\d+\.]+ms) (?<last_bit>.*)$/
+    REXP_RENDER     = /^\s*Rendered (?<path>.*\/)(?<template_name>\w+\.html\.\w+) \((?<rendering_time>[\d+\.]+ms)\)$/
+    REXP_RENDER_WITHIN = /^\s*Rendered (?<path>.*\/)(?<template_name>\w+\.html\.\w+) within (?<layout_name>[\w\d\/]+) ?\((?<rendering_time>[\d+\.]+ms)\)$/
+    REXP_COMPLETE   = /^Completed (?<status>\d+\s\w+) in (?<total_time>[\d+\.]+ms) (?<last_bit>.*)$/
+    REXP_SQL_QUERY  = /^\s*(?<model_load>[\w:]+ Load) \((?<rendering_time>[\d+\.]+ms)\) \s*(?<sql_query>.*)$/
 
     def colorize_line(line)
+      line = strip_ansi_colors(line)
+
       case line
       when REXP_REQUEST then colorize_request(line)
       when REXP_CONTROLLER then colorize_controller(line)
       when REXP_RENDER then colorize_render(line)
+      when REXP_RENDER_WITHIN then colorize_render_within(line)
       when REXP_COMPLETE then colorize_complete(line)
+      when REXP_SQL_QUERY then colorize_sql_query(line)
       else line
       end
     rescue => e
@@ -19,6 +25,10 @@ module Browserlog
     end
 
     private
+
+    def strip_ansi_colors(line)
+      line.gsub(/\e\[(\d+)m/, '')
+    end
 
     def span(item, classname)
       "<span class='#{classname}'>#{item}</span>"
@@ -51,6 +61,24 @@ module Browserlog
       template_name = span(data[:template_name], 'template-name')
       rendering_time = span(data[:rendering_time], 'rendering-time')
       "#{space}Rendered #{path}#{template_name} (#{rendering_time})"
+    end
+
+    def colorize_render_within(line)
+      data = regex_parse(line.match(REXP_RENDER_WITHIN))
+      path = data[:path]
+      layout_name = span(data[:layout_name], 'layout-name')
+      template_name = span(data[:template_name], 'template-name')
+      rendering_time = span(data[:rendering_time], 'rendering-time')
+      "#{space}Rendered #{path}#{template_name} within #{layout_name} (#{rendering_time})"
+    end
+
+    def colorize_sql_query(line)
+      data = regex_parse(line.match(REXP_SQL_QUERY))
+      path = data[:path]
+      model_load = span(data[:model_load], 'model-load')
+      rendering_time = span(data[:rendering_time], 'rendering-time')
+      sql_query = data[:sql_query]
+      "#{space}#{model_load} (#{rendering_time}) #{sql_query}"
     end
 
     def colorize_complete(line)
